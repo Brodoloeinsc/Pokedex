d553cae4487207e5a3888e428c1bc080abe047d3
João <joaofernandovascao@gmail.com>
2024-01-30 12:21:34 -0300

    Add popup de infos especificas

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ function pegaPokemons(quantidade){
                         poke_boxes.innerHTML+=`
                             <div class="pokemon-box">
                             <img src="`+val.imagem+`">
-                            <p>`+val.nome+`</p>
+                            <p onclick="abrir_popup('`+val.nome+`')">`+val.nome+`</p>
                             </div>
                         `
                         /*
@@ -47,4 +47,44 @@ function pegaPokemons(quantidade){
             console.log(val.nome)
         })
     })
+}
+
+var pop_up = document.getElementById('popup');
+var popup_content = document.getElementById('content');
+var classe = document.getElementsByClassName('pokemon-class');
+
+pop_up.addEventListener('click', ()=>{
+    fechar_popup();
+})
+
+function abrir_popup(name){
+    pop_up.style.display = "block";
+    popup_content.style.display = "block";
+
+    fetch('https://pokeapi.co/api/v2/pokemon/'+name)
+    .then(response => response.json())
+    .then(data => {
+        image = data.sprites.other.dream_world.front_default;
+        popup_content.innerHTML += `<img src="`+image+`">`;
+        popup_content.innerHTML += `<h3>`+data.name+`</h3>`;
+
+        data.types.map((tipo)=>{
+            nome = tipo.type.name
+            achar_id = tipo.type.url
+
+            console.log(achar_id);
+
+            fetch(achar_id)
+            .then(response => response.json())
+            .then(result=>{
+                popup_content.innerHTML += `<p class="cor`+result.id+`">`+result.name+`</p><br>`;
+            })
+        })
+    })
+}
+
+function fechar_popup(){
+    pop_up.style.display = "none";
+    popup_content.style.display = "none";
+    popup_content.innerHTML = "";
 }
